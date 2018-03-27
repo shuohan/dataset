0a7914a2d7c68a42e45b876347caaabf48372266
Shuo Han <shan50@jhu.edu>
2018-03-26 20:32:33 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(name='network-utils',
+      version='0.1.0',
+      description='PyTorch utility classes and functions',
+      author='Shuo Han',
+      author_email='shan50@jhu.edu',
+      packages=['network_utils'])
