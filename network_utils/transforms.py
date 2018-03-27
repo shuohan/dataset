7758aaea26c382cf48c8f9dea708aae2f8abc46c
Shuo Han <shan50@jhu.edu>
2018-03-26 20:14:27 -0400

    Test deformation

--- network_utils/transforms.py
+++ network_utils/transforms.py
@@ -7,6 +7,17 @@ from scipy.ndimage.measurements import center_of_mass
 from image_processing_3d import rotate3d, deform3d, calc_random_deformation3d
 
 
+def deform_tripple_3d(images, sigma, scale):
+    shape = images[0].shape
+    x_deformation = calc_random_deformation3d(shape, sigma, scale)
+    y_deformation = calc_random_deformation3d(shape, sigma, scale)
+    z_deformation = calc_random_deformation3d(shape, sigma, scale)
+    image = deform3d(images[0], x_deformation, y_deformation, z_deformation, 1)
+    label = deform3d(images[1], x_deformation, y_deformation, z_deformation, 0)
+    mask = deform3d(images[2], x_deformation, y_deformation, z_deformation, 0)
+    return image, label, mask
+
+
 def random_rotate3d(images, max_angle=5):
     orders = [1, 0, 0]
     point = None
