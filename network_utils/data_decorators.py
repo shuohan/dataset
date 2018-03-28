f53b09ecdd19af1edf1132b5fc56c1bb4baa8118
Shuo Han <shan50@jhu.edu>
2018-03-28 00:14:57 -0400

    Update Transforming3d to use Transformer
    Use Transformer to control the concrete transform. For example,
    (image, label_image) pair should undergo the same transformation;
    if use transform function in Transforming3d and the transform should
    be randomized, there is no convenient way to guarantee the the pair
    have the same tansform. However, using a Transformer, user can control
    when to randomized the transform, therefore, the same transform can be
    applied to the pair.

--- network_utils/data_decorators.py
+++ network_utils/data_decorators.py
@@ -2,6 +2,7 @@
 
 import os
 from image_processing_3d import crop3d, calc_bbox3d, resize_bbox3d
+from image_processing_3d import rotate3d, deform3d, calc_random_deformation3d
 
 from .data import Data, Data3d
 
@@ -66,17 +67,89 @@ class Binarizing3d(DataDecorator):
 
 class Transforming3d(DataDecorator):
 
-    def __init__(self, data, transform, get_data_on_the_fly=True):
-        super().__init__(get_data_on_the_fly)
-        self.transform = transform
+    def __init__(self, data, transformer, get_data_on_the_fly=True):
+        super().__init__(data, get_data_on_the_fly)
+        self.transformer = transformer
+
+    def update(self):
+        self.transformer.update()
     
     def _get_data(self):
-        """Transform data
+        return self.transformer.transform(self.data.get_data())
 
-        Returns:
-            transformed (num_channels x num_i x ... numpy.array): The
-                transformed data
 
-        """
-        transformed = self.transform(self.data.get_data())
-        return transformed
+class Transformer:
+    def update(self):
+        raise NotImplementedError
+    def transform(self, data):
+        raise NotImplementedError
+
+
+class Flipper(Transformer):
+
+    def __init__(self, dim=1, label_pairs=[]):
+        self.dim = dim
+        self.label_pairs = label_pairs
+
+    def update(self):
+        pass
+
+    def transform(self, data):
+        flipped = np.flip(data, self.dim)
+        for (pair1, pair2) in self.label_pairs:
+            flipped[flipped==pair1] = pair2
+            flipped[flipped==pair2] = pair1
+        return flipped
+
+
+class Rotator(Transformer):
+
+    def __init__(self, max_angle=5, point=None, order=1):
+        self.max_angle = max_angle
+        self.point = point
+        self.order = order
+
+        self._rand_state = np.random.RandomState()
+        self.update()
+
+    def update(self):
+        self._x_angle = self._calc_rand_angle()
+        self._y_angle = self._calc_rand_angle()
+        self._z_angle = self._calc_rand_angle()
+
+    def transform(self, data):
+        rotated = rotate3d(data, self._x_angle, self._y_angle, self._z_angle,
+                           point=self.point, order=self.order)
+        return rotated
+
+    def _calc_rand_angle(self):
+        angle = self._rand_state.rand(1)
+        angle = float(angle * 2 * self.max_angle - self.max_angle)
+        return angle
+
+
+class Deformer(Transformer):
+
+    def __init__(self, shape, sigma, scale, order=1):
+        self.sigma = sigma
+        self.scale = scale
+        self.shape = shape
+        self.order = order
+
+        self._rand_state = np.random.RandomState()
+        self.update()
+
+    def update(self):
+        self._x_deform = self._calc_random_deform()
+        self._y_deform = self._calc_random_deform()
+        self._z_deform = self._calc_random_deform()
+
+    def transform(self, data):
+        deformed = deform3d(data, self._x_deform, self._y_deform, self.z_deform,
+                            self.order)
+        return deformed
+
+    def _calc_random_deform():
+        scale = self._rand_state.rand(1) * self.scale
+        deform = calc_random_deformation3d(self.shape, self.sigma, scale)
+        return deform
