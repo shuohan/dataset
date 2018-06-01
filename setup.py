f47f842add337863d0b44076992ba82c165a3644
Shuo Han <shan50@jhu.edu>
2018-06-01 09:04:39 -0400

    Recognize git tag in setup.py as version

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
 
 from distutils.core import setup
+from glob import glob
+import subprocess
+
+command = ['git', 'describe', '--tags']
+version = subprocess.check_output(command).decode().strip()
 
 setup(name='network-utils',
-      version='0.4.0',
+      version=version,
       description='Deep network utility',
       author='Shuo Han',
       author_email='shan50@jhu.edu',
-      packages=['network_utils'])
+      packages=['network_util'])
