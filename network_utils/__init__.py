c6acb762a4ab96f6ac4819144caf2ef13dc5aa9a
Shuo Han <shan50@jhu.edu>
2018-09-19 17:51:51 -0400

    Update __init__ for bounding box

--- network_utils/__init__.py
+++ network_utils/__init__.py
@@ -3,4 +3,5 @@
 from .datasets import Dataset3d, Dataset3dFactory
 from .data_factories import TrainingDataFactory
 from .data_factories import Data3dFactoryDecorator, Data3dFactoryBinarizer
+from .bounding_box import BboxFactoryDecorator
 from .label_image_binarizer import LabelImageBinarizer
