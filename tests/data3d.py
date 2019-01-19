97eec73cc80545595ddd559c1eb34c8ca0a222f6
Shuo Han <shan50@jhu.edu>
2019-01-19 11:51:10 -0500

    Test Data3d

--- tests/data3d.py
+++ tests/data3d.py
@@ -1,46 +1,42 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import sys
-sys.path.insert(0, '..')
-
 import matplotlib.pyplot as plt
 from time import time
+from memory_profiler import profile
 
 from network_utils.data import Data3d
 
-filepath = 'data/AT1000_image.nii.gz'
+filepath = 'data/at1000_image.nii.gz'
 
-data = Data3d(filepath, get_data_on_the_fly=True)
+@profile
+def test(on_the_fly):
+    """Teset Data3d"""
+    print('On the fly:', on_the_fly)
+    data = Data3d(filepath, on_the_fly=on_the_fly)
 
-start_time = time()
-data.get_data()
-end_time = time()
-print('get data on the fly, first load', end_time - start_time)
+    start_time = time()
+    data.get_data()
+    end_time = time()
+    print('First load', end_time - start_time)
 
-start_time = time()
-data.get_data()
-end_time = time()
-print('get data on the fly, second load', end_time - start_time)
+    start_time = time()
+    data.get_data()
+    end_time = time()
+    print('Second load', end_time - start_time)
 
-data = Data3d(filepath, get_data_on_the_fly=False)
+    return data
 
-start_time = time()
-data.get_data()
-end_time = time()
-print('get data once, first load', end_time - start_time)
+if __name__ == "__main__":
+    test(True) 
+    data = test(False)
 
-start_time = time()
-data.get_data()
-end_time = time()
-print('get data once, second load', end_time - start_time)
-
-plt.figure()
-shape = data.get_data().shape[1:]
-plt.subplot(1, 3, 1)
-plt.imshow(data.get_data()[0, shape[0]//2, :, :], cmap='gray')
-plt.subplot(1, 3, 2)
-plt.imshow(data.get_data()[0, :, shape[1]//2, :], cmap='gray')
-plt.subplot(1, 3, 3)
-plt.imshow(data.get_data()[0, :, :, shape[2]//2], cmap='gray')
-plt.show()
+    plt.figure()
+    shape = data.get_data().shape[1:]
+    plt.subplot(1, 3, 1)
+    plt.imshow(data.get_data()[0, shape[0]//2, :, :], cmap='gray')
+    plt.subplot(1, 3, 2)
+    plt.imshow(data.get_data()[0, :, shape[1]//2, :], cmap='gray')
+    plt.subplot(1, 3, 3)
+    plt.imshow(data.get_data()[0, :, :, shape[2]//2], cmap='gray')
+    plt.show()
